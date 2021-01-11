8c78ca6641945d9d0f3bed777dfacf24cd9236f7
Julien Schueller <schueller@phimeca.com>
2021-01-11 16:48:31 +0100

    add

--- main.cxx
+++ main.cxx
@@ -13,18 +13,22 @@ int main(int argc, char *argv[])
     QWidget * widget = new QWidget;
     QHBoxLayout * hbox = new QHBoxLayout;
 
-    QPushButton * button = new QPushButton("show");
-    hbox->addWidget(button);
+    QPushButton * button1 = new QPushButton("set");
+    QPushButton * button2 = new QPushButton("add");
+    hbox->addWidget(button1);
+    hbox->addWidget(button2);
     QStackedWidget * stacked = new QStackedWidget;
     QLabel * label1 = new QLabel("label1");
     QLabel * label2 = new QLabel("label2");
+    QLabel * label3 = new QLabel("label3");
     stacked->addWidget(label1);
     stacked->addWidget(label2);
     hbox->addWidget(stacked);
     widget->setLayout(hbox);
     widget->show();
 
-    QObject::connect(button, &QPushButton::clicked, [=]() { stacked->setCurrentIndex(stacked->currentIndex() ? 0:1); });
+    QObject::connect(button1, &QPushButton::clicked, [=]() { stacked->setCurrentIndex(stacked->currentIndex() ? 0:1); });
+    QObject::connect(button2, &QPushButton::clicked, [=]() { stacked->addWidget(label3);  stacked->setCurrentWidget(label3); });
     
     QTimer *timer = new QTimer;
 //     QObject::connect(timer, SIGNAL(timeout()), &hmw, SLOT(close()));
